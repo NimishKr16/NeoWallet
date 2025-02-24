7527d4dabfb10c5ddb2e85185a746077470c3e8f
Nimish_Kumar <nimish.kumar@bluphlux.com>
2025-02-25 02:12:23 +0530

    Setup env

--- neo-wallet/src/lib/firebaseConfig.ts
+++ neo-wallet/src/lib/firebaseConfig.ts
@@ -10,13 +10,13 @@ import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAM_q0NYduClRJiWcTQCsqKbmHgfCuzkbs",
-  authDomain: "neo-wallet-8be73.firebaseapp.com",
-  projectId: "neo-wallet-8be73",
-  storageBucket: "neo-wallet-8be73.firebasestorage.app",
-  messagingSenderId: "549254710386",
-  appId: "1:549254710386:web:caceecb4c3c48f5a516eeb",
-  measurementId: "G-QRZ34MPCQL"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
@@ -24,3 +24,4 @@ const app = initializeApp(firebaseConfig);
 // const analytics = getAnalytics(app);
 export const auth = getAuth(app);
 export const db = getFirestore(app);
+
